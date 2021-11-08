1f6d1030acfaa0a0a0abc8c9e951552dcd9667bc
Connor Hughes <seanandhughes@gmail.com>
2021-11-08 20:36:46 +0000

    finds long url given the shrot url as a key

--- express_server.js
+++ express_server.js
@@ -21,7 +21,7 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL,
-    longURL: `https://www.${req.params.shortURL}`
+    longURL: urlDatabase[req.params.shortURL]
   };
   res.render('urls_show', templateVars);
 });
